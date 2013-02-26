c4cda32686b162ac8d751dadca800d6e272689a5
Kirsanov_Aleksandr <kashmervil@gmail.com>
2013-02-26 17:48:38 +0400

    ololo

--- semestr_4/1/task_1_1.hs
+++ semestr_4/1/task_1_1.hs
@@ -2,6 +2,6 @@ fact :: (Integral n) => n -> n
 fact 0 = 1
 fact n = n*fact(n-1)
 main = do
-putStr "Factorial of n = \n"
-n <- getLine
-putStrLn ("n! = " ++ (show $fact(read n :: Integer)))
+    putStr "Factorial of n = \n"
+    n <- getLine
+    putStrLn ("n! = " ++ (show $fact(read n :: Integer)))
