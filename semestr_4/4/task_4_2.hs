3ca8e7b7e93adb7025114f749930fe2ac1d41fb3
Kirsanov_Aleksandr <kashmervil@gmail.com>
2013-03-13 01:29:55 +0400

    folded trees
    =)

---
+++ semestr_4/4/task_4_2.hs
@@ -0,0 +1,14 @@
+data Tree a = EmptyTree
+            | Node a (Tree a) (Tree a)
+			deriving (Show, Eq) 
+ 
+treeInsert :: (Ord a) => a -> Tree a -> Tree a  
+treeInsert x EmptyTree = Node x EmptyTree EmptyTree 
+treeInsert x (Node a left right)   
+    | x == a = Node x left right  
+    | x < a  = Node a (treeInsert x left) right  
+    | x > a  = Node a left (treeInsert x right)
+	
+foldt :: ( a -> a -> a) -> a -> Tree a -> a
+foldt _ z EmptyTree = z
+foldt f z (Node x left right) = foldt f (f x (foldt f z left)) right
