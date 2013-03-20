c32bb3cead69dc6d5091a69a2aa7ca3636d938e0
Kirsanov_Aleksandr <kashmervil@gmail.com>
2013-03-20 22:39:41 +0400

    true fold

--- semestr_4/4/task_4_2.hs
+++ semestr_4/4/task_4_2.hs
@@ -9,6 +9,6 @@ treeInsert x (Node a left right)
     | x < a  = Node a (treeInsert x left) right  
     | x > a  = Node a left (treeInsert x right)
 	
-foldt :: ( a -> a -> a) -> a -> Tree a -> a
+foldt :: ( a -> b -> b) -> b -> Tree a -> b
 foldt _ z EmptyTree = z
 foldt f z (Node x left right) = foldt f (f x (foldt f z left)) right
