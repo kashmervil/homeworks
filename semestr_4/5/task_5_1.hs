59eea0db94c4609893c9a6a2f144052d0328d88a
kashmervil <kashmervil@gmail.com>
2013-04-18 17:01:10 +0400

    partitions for dummies

--- semestr_4/5/task_5_1.hs
+++ semestr_4/5/task_5_1.hs
@@ -1,4 +1,8 @@
 import Control.Monad
+import Data.List
+helper :: Int -> [[Int]]
+helper 0 = [[]]
+helper n = [1..n] >>= (\x -> map (x:) (par (n - x)))
 
-par 0 = [[]]
-par n = [1..n] >>= (\x -> map (x:) (par (n - x)))
+par :: Int -> [[Int]]
+par x = nub $ map sort $ helper x
